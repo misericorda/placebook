6eb7b193f19b10aeee8d4b5108d7f9a4e53fbfde
Dmitry Oleynik <johnkeats2011@gmail.com>
2018-08-20 19:19:37 +0300

    Installed maps

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
 
   componentDidMount() {
-    ImagePicker.showImagePicker({title: 'Pick an Image', maxWidth: 800, maxHeight: 600}, res => {
-      if (res.didCancel) {
-        console.log('User cancelled!');
-      } else if (res.error) {
-        console.log('Error', res.error);
-      } else {
-        console.log('Success!');
-      }
-    })
+    // ImagePicker.showImagePicker({title: 'Pick an Image', maxWidth: 800, maxHeight: 600}, res => {
+    //   if (res.didCancel) {
+    //     console.log('User cancelled!');
+    //   } else if (res.error) {
+    //     console.log('Error', res.error);
+    //   } else {
+    //     console.log('Success!');
+    //   }
+    // })
   }
 
   render() {
@@ -22,6 +23,18 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <MapView style={{
+          width: '100%',
+          height: 200,
+        }}
+                 initialRegion={{
+                   latitude: 37.78825,
+                   longitude: -122.4324,
+                   latitudeDelta: 0.0922,
+                   longitudeDelta: 0.0421,
+                 }}
+        />
+        <Text>HI</Text>
       </View>
     );
   }
