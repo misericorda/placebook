1291f8287e6dc62f9806b25b72443e5786d9c86c
Dmitry Oleynik <johnkeats2011@gmail.com>
2018-08-28 11:57:32 +0300

    removed hardcoded urls and keys from constants

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,6 @@
-export const DATABASE_URL = 'https://memento-1530788482372.firebaseio.com';
-export const STORE_IMAGE_URL = 'https://us-central1-memento-1530788482372.cloudfunctions.net/storeImage';
+export {FIREBASE_API_KEY, DATABASE_URL, STORE_IMAGE_URL} from '../credentials';
 
 export const STORAGE_TOKEN_NAME = 'placebook:auth:token';
 export const STORAGE_TOKEN_EXP_DATE_NAME = 'placebook:auth:expiryDate';
 export const STORAGE_TOKEN_REFRESH_NAME = 'placebook:auth:refreshToken';
 export const STORAGE_USER_ID = 'placebook:auth:uid';
-
-export const FIREBASE_API_KEY = 'AIzaSyC1CZ1S4grsDPMYrBjumJ-QZxjl2sRdmgE';
